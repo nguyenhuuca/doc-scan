ce905fff2eb8f3c6e0441d51ad81645189b7514f
nguyenhuuca <nguyenhuuca@gmail.com>
2026-07-02 22:18:14 +0700

    chore: bump version to 1.5.2, update release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.docscanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.5.1"
+        versionCode = 8
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
